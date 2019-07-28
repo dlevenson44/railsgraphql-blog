0460d432ac8c846db39d729647efbc0dfd41147c
dlevenson44 <daniellevenson4@gmail.com>
2019-07-28 10:13:44 -0400

    Setup comment model and migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_023358) do
+ActiveRecord::Schema.define(version: 2019_07_28_140428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "body"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 User.destroy_all
 Post.destroy_all
+Comment.destroy_all
 
 one = User.create!(first_name: "dan", last_name: "levenson", street: 'madison', number: 500, city: 'NYC', zipcode: 55555, country: 'USA')
 two = User.create!(first_name: "sue", last_name: "progoff", street: 'park', number: 50, city: 'NYC', zipcode: 55555, country: 'USA')
@@ -8,3 +9,12 @@ post_one = Post.create!(user_id: 1, body: 'test body 1')
 post_two = Post.create!(user_id: 1, body: 'test body 1 a')
 post_three = Post.create!(user_id: 2, body: 'test body 2')
 post_four = Post.create!(user_id: 2, body: 'test body 2 a')
+
+comment_one = Comment.create!(post_id: 1, body: 'test comment 1')
+comment_two = Comment.create!(post_id: 1, body: 'test comment 1 a')
+comment_three = Comment.create!(post_id: 2, body: 'test comment 2')
+comment_four = Comment.create!(post_id: 2, body: 'test comment 2 a')
+comment_five = Comment.create!(post_id: 3, body: 'test comment 3')
+comment_six = Comment.create!(post_id: 3, body: 'test comment 3 a')
+comment_seven = Comment.create!(post_id: 4, body: 'test comment 4')
+comment_eight = Comment.create!(post_id: 4, body: 'test comment 4 a')

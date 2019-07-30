0abe9c53ab3f3149d88e2c72a9a336e2fbde7686
dlevenson44 <daniellevenson4@gmail.com>
2019-07-29 21:21:04 -0400

    Update models to validate needed fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  validates :first_name, :last_name, presence: true
 
   def full_address
     ([number, street, zipcode, city, country].compact).join ', '
